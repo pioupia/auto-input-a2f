16ab908c3c45329d2a679b7213ce785b35c3226b
PIoupia <pioupiadeux@gmail.com>
2021-08-09 22:42:23 +0200

    Update 1.0.2
    - Adding options 
    - Adding new functions

--- README.md
+++ README.md
@@ -40,7 +40,39 @@ Or non minified :
 
 ## Usage
 
+If you dont want to put all the html code yourself, a function is available in javascript that will create all the code for you in one line.
+
+Init the package :
+```js
+initAutoInput(options, callback);
+```
+
+The callback is optional, it's if you dont want to assigned an atribute to your button of validation.
+
+| Options  | Description | Default | Type |
+| ------------- | ------------- | ------------- | ------------- |
+| autoend  | Activates the automatic end. Notify you when the user has completely filled the captcha via the callback or by pressing your element to signal the end. | true | Boolean |
+| selectAuto  | Automatically selects the first input to be filled in when the user arrives on the page  | true | Boolean |
+| canPast | Allows user to paste a2f code | true | Boolean |
+| createAuto | Automatically creates all the html code required for validation of the a2f. | false | Boolean |
+| parent | Only if the createAuto option is activated. Defines the parent of the automatically generated html code. May not be enabled if and only if the parent contains the attribute : "data-parent-a2f" | get the attribute "data-parent-a2f" OR get the id #a2fParent | HTML element |
+
+
+If you have verified the a2f code and he's not valid, you can reset all the values with this function :
+
+```js
+deleteNumbersCode();
+```
+
+If you want to get the actual a2f code of the user, you can call this function :
+```js
+getNumbersCode() // => 123456
+```
+
 #### HTML
+
+(Only if the createAuto option is not activated)
+
 ```html
 <input data-a2f type="number" min="0" max="9" placeholder="0" required>
 <input data-a2f type="number" min="0" max="9" placeholder="0" required>
@@ -50,15 +82,52 @@ Or non minified :
 <input data-a2f type="number" min="0" max="9" placeholder="0" required>
 ```
 
-If you want the plugin to automatically validate the a2f, put on your html element with the onlick eventlistener :
+If you want the plugin to automatically validate the a2f, put on your html element with the onlick eventlistener (only with autoend option activated) :
+
 ```html
 <button onclick="myFunction()" data-button-validate>Send my A2F code</button>
+```
+OR use the callback function like that :
+
+```js
+initAutoInput(options, myFunction);
 ```
 
 #### JS
 ```js
+// Without callback
 function myFunction(){
-  const a2fcode = getNumbersCode();
+  const a2fcode = getNumbersCode(); 
+  // -> 123456 (int)
   // Check your a2f code if you want here.
+}
+
+
+// With callback
+function myFunction(code){
+  // code => 123456 (int)
+  // Check your a2f code if you want here.
+}
+```
+
+#### Exemple 
+
+```html
+<div class="container">
+  <div data-parent-a2f>
+  </div>
+  <button id="sendA2F" onclick="myFunctionTest">Send</button>
+</div>
+<script src="https://pioupia.github.io/auto-input-a2f/autoinput.min.js"></script>
+```
+
+```js
+initAutoInput({ createAuto: true }, myFunctionTest);
+
+function myFunctionTest(code){
+    if(typeof code !== 'number') code = getNumbersCode();
+    console.log(code) // 123456
+
+    if(!check(code)) return deleteNumbersCode();
 }
 ```
--- dist/autoinput.js
+++ dist/autoinput.js
@@ -1,38 +1,72 @@
 /* Create By Pioupia https://github.com/pioupia/auto-input-a2f/ | MIT License */
-(() => {
-    const allBoxes = document.querySelectorAll("[data-a2f]");
-    if((allBoxes?.length||0)<2) return;
-    let i = 0;
-    allBoxes.forEach(e => {
-        e.oninput = () => {
-            if(isNaN(e.value)) e.value = 0;
-            if(e.value < 0) e.value = 0;
-            if(e.value > 9) e.value = 9;
-            if(i+1 > allBoxes.length){
-                let canValidate = true;
-                for(let o = 0; o < allBoxes.length; o++){
-                    if(!allBoxes[o].value){
-                        canValidate = allBoxes[o];
-                        break;
-                    }
-                }
-                if(canValidate === true){
-                    document.querySelector('[data-button-validate]')?.click();
-                }else{
-                    canValidate.focus();
-                }
-            }
-        }
-        i++;
-    })
-})()
+let allBoxes = [];
+function initAutoInput(options, callback){
+    typeof options.autoend != 'boolean' ? options.autoend = true : '';
+    typeof options.selectAuto != 'boolean' ? options.selectAuto = true : '';
+    typeof options.canPast != 'boolean' ? options.canPast = true : '';
+    typeof options.createAuto != 'boolean' ? options.createAuto = false : '';
+    options.parent ||= document.querySelector("[data-parent-a2f]") || 'a2fParent';
 
+    if(options.createAuto){
+        const parent = document.getElementById(options.parent);
+        if(!parent) return console.error("Parent does not exist.");
+        for(let i = 0; i < 6; i++){
+            const e = document.createElement("input");
+            e.setAttribute("data-a2f",'');
+            e.setAttribute("type",'text');
+            e.setAttribute("placeholder",'0');
+            e.setAttribute('required','');
+            if(i == 3){
+                const a = document.createElement("span");
+                a.innerHTML = "-";
+                parent.appendChild(a);
+            }
+            parent.appendChild(e)
+        }
+    }
+    allBoxes = document.querySelectorAll("[data-a2f]");
+    let array = [];
+    allBoxes.forEach(e => array.push(e));
+    if((allBoxes?.length||0)<2) return;
+    if(options.selectAuto) allBoxes[0].focus();
+    for(let i = 0; i < allBoxes.length; i++){
+        const e = allBoxes[i];
+        if(options.canPast) e.onpaste = t => handlePaste(t);
+        e.onkeyup = (t) => {
+            if(t.keyCode == 16)return;
+            if(t.keyCode == 8) return e.value = '';
+            if(t.keyCode == 46) e.value = '';
+            if(t.keyCode == 46 || t.keyCode == 37) return allBoxes[i-1]?.focus();
+            if(isNaN(t.key)) return e.value = '';
+            e.value = t.key;
+            if(array.filter(r => r.value).length === allBoxes.length && options.autoend){
+                if(callback) return callback(getNumbersCode());
+                return document.querySelector('[data-button-validate]')?.click();
+            }
+            allBoxes[i+1]?.focus();
+        }
+    };
+}
+
+function handlePaste(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    const clipboardData = e.clipboardData || window.clipboardData;
+    if(clipboardData){
+        const d = clipboardData.getData("Text").split('');
+        for(let i = 0; i < d.length; i++){
+            if(allBoxes[i]) allBoxes[i].value = d[i];
+        }
+    }
+}
+function deleteNumbersCode(){
+    allBoxes.forEach(e =>  e.value = '0');
+}
 function getNumbersCode(){
-    const allBoxes = document.querySelectorAll("[data-a2f]");
     if((allBoxes?.length||0)<2) return "Too little input reported";
     let code = '';
     allBoxes.forEach(e => {
         code += e.value;
     });
-    return code;
+    return parseInt(code);
 }
